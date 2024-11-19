e36bb4dd2901953085d0433318bd3974130f4962
Milee <heszmilan@gmail.com>
2024-11-19 10:39:30 +0100

    Add project files.

---
+++ berek2020/Adatok.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek2020
+{
+    internal class Adatok
+    {
+        public string Nev { get; set; }
+        public bool Nem { get; set; }
+        public string Szakma { get; set; }
+        public int Belepes { get; set; }
+        public int Ber { get; set; }
+
+        public Adatok(string sor)
+        {
+            var v = sor.Split(';');
+            Nev = v[0];
+            Nem = v[1] == "férfi";
+            Szakma = v[2];
+            Belepes = int.Parse(v[3]);
+            Ber = int.Parse(v[4]);
+        }
+        public override string ToString()
+        {
+            return $"Név: {Nev} | Neme: {(Nem ? "férfi" : "nő")} | Szakma: {Szakma} | Belépés: {Belepes} | Bér: {Ber}";
+        }
+    }
+}
---
+++ berek2020/Program.cs
@@ -0,0 +1,54 @@
+﻿using berek2020;
+using System.Text;
+
+const string FILE = @"..\..\..\src\berek2020.txt";
+List<Adatok> adatok = new();
+
+using StreamReader sr = new(FILE, Encoding.UTF8);
+_=sr.ReadLine();
+while (!sr.EndOfStream) adatok.Add(new(sr.ReadLine()));
+
+
+foreach (Adatok i in adatok)
+{
+    Console.WriteLine(i.ToString());
+}
+
+Console.WriteLine($"\nEnnyi dogozó adatai találhatóak: {adatok.Count}");
+
+var f2 = adatok
+    .Average(x => x.Ber);
+var kerekites = Math.Round(f2);
+
+Console.WriteLine($"\n2020 átlag bér: {kerekites}");
+
+
+Console.Write("\nÍrjon be egy részleget: ");
+string bekertReszleg = Console.ReadLine();
+
+var legnagyobbBer = adatok
+    .Where(x => x.Szakma == bekertReszleg)
+    .OrderByDescending(x => x.Ber)
+    .FirstOrDefault();
+
+if (legnagyobbBer == null)
+{
+    Console.WriteLine("\nA megadott részleg nem létezik a cégnél");
+}
+else
+{
+    Console.WriteLine($"\nFeladat: A legnagyobb bérrel rendelkező {bekertReszleg} részlegen dolgozó:");
+    Console.WriteLine(legnagyobbBer);
+}
+
+Console.WriteLine("\nStatisztika az egyes részlegeken dolgozók számáról:");
+var statisztika = adatok
+    .GroupBy(x => x.Szakma)
+    .Select(x => new { Reszleg = x.Key, DolgozokSzama = x.Count() })
+    .OrderBy(x => x.Reszleg);
+
+foreach (var reszlegStat in statisztika)
+{
+    Console.WriteLine($"\t{reszlegStat.Reszleg}: {reszlegStat.DolgozokSzama} fő");
+}
+
